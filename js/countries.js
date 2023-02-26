35450c11cd924185c6942e77e75e4d170dfab3a6
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-02-27 00:47:42 +0600

    country final

--- js/countries.js
+++ js/countries.js
@@ -1,5 +1,24 @@
 const loadCountries = () => {
     fetch('https://restcountries.com/v3.1/all')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayCountries(data))
 }
+loadCountries();
+
+const displayCountries = countries => {
+    // for(country of countries){
+    // }
+    const countriesDiv = document.getElementById('countries');
+    countries.forEach(country => {
+        console.log(country)
+        const div = document.createElement('div');
+        div.classList.add('country');
+        const h3 = document.createElement('h3');
+        h3.innerText = `Name: ${country.name.common}`;
+        div.appendChild(h3);
+        const p = document.createElement('p');
+        p.innerText = `Capital: ${ country.capital}`;
+        div.appendChild(p);
+        countriesDiv.appendChild(div);
+    });
+} 
