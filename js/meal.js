c0504d14fc59d51168c455333a600666d7f6a043
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-03-06 23:47:44 +0600

    details

--- js/meal.js
+++ js/meal.js
@@ -14,11 +14,11 @@ const searchFood = () => {
 const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result');
     meals.forEach(meal => {
-        console.log(meal);
+        // console.log(meal);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100">
+        <div onclick="loadMealDetails(${meal.idMeal})" class="card h-100">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">${meal.strMeal}</h5>
@@ -30,4 +30,17 @@ const displaySearchResult = meals => {
           </div>`;
           searchResult.appendChild(div);
     });
+}
+
+const loadMealDetails = mealId => {
+  // console.log(mealId);
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+  fetch(url)
+  .then( res => res.json())
+  .then(data => displayMealDetail(data.meals[0]));
+}
+
+const displayMealDetail = meal =>{
+  console.log(meal);
+  const mealDetails = document.getElementById
 }
